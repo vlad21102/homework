0506b50871c4d8fcbc723a24c7e18491089e9150
Vladislav Kapitonov <v9030590@gmail.com>
2026-06-08 13:21:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,45 +4,75 @@
 #include <cstdlib>
 #include <iostream>
 
+/**
+ * @brief Считывает вещественное число с клавиатуры
+ * @param text сообщение для пользователя
+ * @return введённое значение
+ */
+double getValue(const char* text);
+
+/**
+ * @brief Точка входа в программу
+ * @return Если программа выполнена коректно - 0, иначе - 1
+ */
 int main()
 {
     system("chcp 1251");
     system("cls");
 
-    double centerX;
-    double centerY;
-    double centerZ;
-    double radius;
-    double height;
+    const double centerX =
+        getValue("Введите координату X центра цилиндра: ");
+    const double centerY =
+        getValue("Введите координату Y центра цилиндра: ");
+    const double centerZ =
+        getValue("Введите координату Z центра цилиндра: ");
 
-    std::cout << "Введите координаты центра цилиндра: ";
-    std::cin >> centerX >> centerY >> centerZ;
-
-    std::cout << "Введите радиус и высоту цилиндра: ";
-    std::cin >> radius >> height;
+    const double radius =
+        getValue("Введите радиус цилиндра: ");
+    const double height =
+        getValue("Введите высоту цилиндра: ");
 
     const Point center(centerX, centerY, centerZ);
     const Cylinder cylinder(center, radius, height);
 
-    std::cout << "Объём цилиндра: " << cylinder.GetVolume() << std::endl;
+    std::cout << "Объём цилиндра: "
+              << cylinder.GetVolume()
+              << std::endl;
 
-    double pointX;
-    double pointY;
-    double pointZ;
-
-    std::cout << "Введите координаты точки: ";
-    std::cin >> pointX >> pointY >> pointZ;
+    const double pointX =
+        getValue("Введите координату X точки: ");
+    const double pointY =
+        getValue("Введите координату Y точки: ");
+    const double pointZ =
+        getValue("Введите координату Z точки: ");
 
     const Point point(pointX, pointY, pointZ);
 
     if (cylinder.IsPointInside(point))
     {
-        std::cout << "Точка лежит внутри цилиндра" << std::endl;
+        std::cout << "Точка лежит внутри цилиндра"
+                  << std::endl;
     }
     else
     {
-        std::cout << "Точка не лежит внутри цилиндра" << std::endl;
+        std::cout << "Точка не лежит внутри цилиндра"
+                  << std::endl;
     }
 
     return 0;
+}
+
+double getValue(const char* text)
+{
+    double value;
+
+    std::cout << prompt << std::endl;
+    std::cin >> value;
+    if (cin.fail())
+    {
+        std::cout << "Ошибка ввода" << std::endl;
+        exit(1);
+    }
+
+    return value;
 }
